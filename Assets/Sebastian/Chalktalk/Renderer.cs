0b2f2d08e4c3fe2ac3b63e353a8ff27487c64693
Wenbo Lan <info@wenbolan.com>
2017-07-04 15:46:10 -0400

    adapt to the new format of the displaylist, need to debug the transform part of it

--- Assets/Sebastian/Chalktalk/Renderer.cs
+++ Assets/Sebastian/Chalktalk/Renderer.cs
@@ -3,93 +3,146 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Chalktalk {
-  public class Renderer : Holojam.Tools.Trackable {
+namespace Chalktalk
+{
+    public class Renderer : Holojam.Tools.Trackable
+    {
 
-		// For DevDebug
-		public Byte[] DataViewer;
+        // For DevDebug
+        public Byte[] DataViewer;
 
-    [SerializeField]
-    private Curve curvePrefab;
-    [SerializeField]
-    private BindingBox bindingBox;
+        [SerializeField]
+        private Curve curvePrefab;
+        [SerializeField]
+        private BindingBox bindingBox;
 
-    [SerializeField]
-    private string label = "Display";
+        [SerializeField]
+        private string label = "Display";
 
-    private List<Curve> curves = new List<Curve>();
+        private List<Curve> curves = new List<Curve>();
 
-    public override string Label {
-      get { return label; }
-    }
-
-    public override string Scope {
-      get { return ""; }// "Chalktalk"; }
-    }
-
-    protected override void UpdateTracking() {
-      if (this.Tracked) {
-        DestroyCurves();
-		DataViewer = data.bytes;
-        Parse(data.bytes);
-        Draw();
-      }
-    }
-
-    private void DestroyCurves() {
-      foreach (Curve curve in curves) {
-        DestroyImmediate(curve.gameObject);
-      }
-      curves.Clear();
-    }
-
-    private void Parse(byte[] bytes) {
-      int cursor = 8;
-      int curveCount = Utility.ParsetoInt16(bytes, cursor);
-      cursor += 2;
-
-      for (int i = 0; i < curveCount; i++) {
-        int length = Utility.ParsetoInt16(bytes, cursor);
-        cursor += 2;
-
-        Color color = Utility.ParsetoColor(bytes, cursor);
-        cursor += 4;
-
-        float width = Utility.ParsetoFloat(Utility.ParsetoInt16(bytes, cursor));
-        cursor += 2;
-
-        List<Vector3> points = new List<Vector3>();
-        for (int j = 0; j < length; j++) {
-          Vector3 point = Utility.ParsetoVector3(bytes, cursor, 1);
-          //point.Scale(bindingBox.transform.localScale);
-          points.Add(bindingBox.transform.rotation * point + bindingBox.transform.position);
-          cursor += 6;
+        public override string Label
+        {
+            get { return label; }
         }
+
+        public override string Scope
+        {
+            get { return ""; }// "Chalktalk"; }
+        }
+
+        protected override void UpdateTracking()
+        {
+            if (this.Tracked)
+            {
+                DestroyCurves();
+                DataViewer = data.bytes;
+                Parse(data.bytes);
+                Draw();
+            }
+        }
+
+        private void DestroyCurves()
+        {
+            foreach (Curve curve in curves)
+            {
+                DestroyImmediate(curve.gameObject);
+            }
+            curves.Clear();
+        }
+
+        private void Parse(byte[] bytes)
+        {
+            //Skip the "CTDATA01" String header
+            int cursor = 8;
+
+            // The total number of words in this packet, then get the size of the bytes size
+            int curveCount = Utility.ParsetoInt16(bytes, cursor);
+            cursor += 2;
+
+            for (; cursor < bytes.Length; )
+            {
+                Debug.Log("Current Cursor: " + cursor);
+                //The length of the current line
+                int length = Utility.ParsetoInt16(bytes, cursor);
+                cursor += 2;
+
+                // if the line data is less than 12, we skip this one curve
+                // TODO: implement the new curve module so that we can keep the curve with the same id
+                if (length < 12)
+                    continue;
+
+
+                // The ID of current line
+                int ID = Utility.ParsetoInt16(bytes, cursor);
+                cursor += 2;
+
+                //Parse the color of the line
+                Color color = Utility.ParsetoColor(bytes, cursor);
+                cursor += 4;
+
+                //float width = Utility.ParsetoFloat(Utility.ParsetoInt16(bytes, cursor));
+                //cursor += 2;
+
+                //Parse the Transform of this Curve
+                Vector3 translation = Utility.ParsetoVector3(bytes, cursor, 1);
+                cursor += 6;
+                Quaternion rotation = Utility.ParsetoQuaternion(bytes, cursor,1);
+                cursor += 6;
+                float scale = Utility.ParsetoFloat(Utility.ParsetoInt16(bytes, cursor));
+                cursor += 2;
+
+                //Parse the type of the stroke
+                int type = Utility.ParsetoInt16(bytes, cursor);
+                cursor += 2;
+
+
+                //Parse the width of the line
+                float width = 0;
+
+                List<Vector3> points = new List<Vector3>();
+                Debug.Log("Current Line's points count: " + (length-12) / 4);
+                Debug.Log("Current Cursor before read the points :" + cursor);
+                for (int j = 0; j < (length - 12) / 4 ; j++)
+                {
+                    Vector3 point = Utility.ParsetoVector3(bytes, cursor, 1);
+                    //point.Scale(bindingBox.transform.localScale);
+                    //Move point to the bindingBox Coordinate
+                    point = bindingBox.transform.rotation * point + bindingBox.transform.position;
+                    //Apply the point transform for each point
+                    points.Add(point );
+                    //points.Add((rotation * point + translation) * scale);
+                    cursor += 6;
+                    width = Utility.ParsetoFloat(Utility.ParsetoInt16(bytes, cursor));
+                    cursor += 2;
+                }
 
                 // bold the framework
                 bool isFrame = boldenFrame(points);
-               // width *= (isFrame) ? 20.0f : 1.0f;
+                // width *= (isFrame) ? 20.0f : 1.0f;
 
                 Curve curve = GameObject.Instantiate<Curve>(curvePrefab);
-        curve.transform.SetParent(this.transform);
+                curve.transform.SetParent(this.transform);
 
-        curve.points = points;
-        curve.width = width;
-        curve.color = isFrame ? new Color(1,1,1,1) : color;
-        curves.Add(curve);
-      }
-    }
+                curve.points = points;
+                curve.width = width;
+                curve.color = isFrame ? new Color(1, 1, 1, 1) : color;
+                curves.Add(curve);
+            }
+        }
 
 
-    private void Draw() {
-      foreach (Curve curve in curves) {
-        curve.Draw();
-      }
-    }
+        private void Draw()
+        {
+            foreach (Curve curve in curves)
+            {
+                curve.Draw();
+            }
+        }
 
         private bool boldenFrame(List<Vector3> points)
         {
-            if(points.Count == 5)
+            if (points.Count == 5)
             {
                 for (int i = 0; i < points.Count; i++)
                 {
--- Assets/Sebastian/Chalktalk/Utility.cs
+++ Assets/Sebastian/Chalktalk/Utility.cs
@@ -3,49 +3,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Chalktalk {
-  public class Utility {
-    public static int ParsetoInt16(byte[] value, int index) {
-      return BitConverter.ToInt16(value, index) < 0 ? BitConverter.ToInt16(value, index) + 0x10000 : BitConverter.ToInt16(value, index);
-    }
+namespace Chalktalk
+{
+    public class Utility
+    {
+        public static int ParsetoInt16(byte[] value, int index)
+        {
+            return BitConverter.ToInt16(value, index) < 0 ? BitConverter.ToInt16(value, index) + 0x10000 : BitConverter.ToInt16(value, index);
+        }
 
-    public static float ParsetoFloat(int number) {
-      return (float)number / 65535f * 2.0f - 1.0f;
-    }
+        public static float ParsetoFloat(int number)
+        {
+            return (float)number / 65535f * 2.0f - 1.0f;
+        }
 
-        public static int ParsetoUInt16(byte[]value, int index)
+        public static int ParsetoUInt16(byte[] value, int index)
         {
             return BitConverter.ToUInt16(value, index);
         }
 
-    public static Color ParsetoColor(byte[] value, int index) {
-      int r, g, b, a;
+        public static Color ParsetoColor(byte[] value, int index)
+        {
+            int r, g, b, a;
             //Debug.Log("color:" + BitConverter.ToUInt16(value, index) + "\t" + (BitConverter.ToUInt16(value, index+2) + 0x10000));
-      r = ParsetoUInt16(value, index) >> 8;
-      g = ParsetoUInt16(value, index) & 0x00ff;
-      b = ParsetoUInt16(value, index + 2) >> 8;
-      a = ParsetoUInt16(value, index + 2) & 0x00ff;
-      return new Color((float)r / 256f, (float)g / 256f, (float)b / 256f, (float)a / 256f);
-    }
+            r = ParsetoUInt16(value, index) >> 8;
+            g = ParsetoUInt16(value, index) & 0x00ff;
+            b = ParsetoUInt16(value, index + 2) >> 8;
+            a = ParsetoUInt16(value, index + 2) & 0x00ff;
+            return new Color((float)r / 256f, (float)g / 256f, (float)b / 256f, (float)a / 256f);
+        }
 
-    public static List<Vector3> ParsetoVector3s(byte[] value, int index, int size) {
-      List<Vector3> rst = new List<Vector3>();
-      for (int i = 0; i < size; i++) {
-        float x = ParsetoFloat(ParsetoInt16(value, index + i * 6)) * 5;
-        float y = ParsetoFloat(ParsetoInt16(value, index + i * 6 + 2)) * 5;
-        float z = ParsetoFloat(ParsetoInt16(value, index + i * 6 + 4)) * 5;
-        rst.Add(new Vector3(x, y, z));
-      }
-      return rst;
-    }
+        public static List<Vector3> ParsetoVector3s(byte[] value, int index, int size)
+        {
+            List<Vector3> rst = new List<Vector3>();
+            for (int i = 0; i < size; i++)
+            {
+                float x = ParsetoFloat(ParsetoInt16(value, index + i * 6)) * 5;
+                float y = ParsetoFloat(ParsetoInt16(value, index + i * 6 + 2)) * 5;
+                float z = ParsetoFloat(ParsetoInt16(value, index + i * 6 + 4)) * 5;
+                rst.Add(new Vector3(x, y, z));
+            }
+            return rst;
+        }
 
-    public static Vector3 ParsetoVector3(byte[] value, int index, float scale) {
-      int ix = ParsetoInt16(value, index);
-      float x = ParsetoFloat(ParsetoInt16(value, index)) * scale;
-      float y = ParsetoFloat(ParsetoInt16(value, index + 2)) * scale;
-      float z = ParsetoFloat(ParsetoInt16(value, index + 4)) * scale;
-      return new Vector3(x, y, z);
+        public static Quaternion ParsetoQuaternion(byte[] value, int index, float scale)
+        {
+            float x = ParsetoFloat(ParsetoInt16(value, index)) * scale;
+            float y = ParsetoFloat(ParsetoInt16(value, index + 2)) * scale;
+            float z = ParsetoFloat(ParsetoInt16(value, index + 4)) * scale;
+            float w = Mathf.Sqrt(1.0f - x * x - y * y - z * z);
+            return new Quaternion(x, y, z, w);
+        }
+        public static Vector3 ParsetoVector3(byte[] value, int index, float scale)
+        {
+            int ix = ParsetoInt16(value, index);
+            float x = ParsetoFloat(ParsetoInt16(value, index)) * scale;
+            float y = ParsetoFloat(ParsetoInt16(value, index + 2)) * scale;
+            float z = ParsetoFloat(ParsetoInt16(value, index + 4)) * scale;
+
+            return new Vector3(x, y, z);
+        }
     }
-  }
 }
 
