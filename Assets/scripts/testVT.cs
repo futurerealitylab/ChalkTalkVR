18edefdd16b19543f9ffb7f3a25c043ac23b93ac
Zhenyi He <snowymohehe@gmail.com>
2018-03-06 23:59:48 -0500

    workable version for verizon, imu sensor fusion does not work

---
+++ Assets/scripts/testVT.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testVT : MonoBehaviour {
+
+    public Transform source;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 sourcePosition = new Vector3(-source.position.x, source.position.y, source.position.z);
+        Quaternion sourceRotation = new Quaternion(source.rotation.x, source.rotation.y, source.rotation.z, source.rotation.w);
+        sourcePosition += sourceRotation * offset;
+        transform.position = sourcePosition;
+    }
+}
