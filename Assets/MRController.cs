33a87ad5a992422b18e9a525c5a124387d289ac2
Zhenyi He <snowymohehe@gmail.com>
2018-03-07 02:15:09 -0500

    remove vive related code and add ARCore template

---
+++ Assets/MRController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class MRController : MonoBehaviour {
+    public Camera FirstPersonCamera;
+    public GameObject TrackedPlanePrefab;
+    public GameObject ChalkTalkPrefab;
+    public GameObject SearchingForPlaneUI;
+
+    private List<TrackedPlane> m_NewPlanes = new List<TrackedPlane>();
+    private List<TrackedPlane> m_AllPlanes = new List<TrackedPlane>();
+
+    private bool m_IsQuitting = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
