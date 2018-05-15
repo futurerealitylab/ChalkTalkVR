1b1602db6802093cb9096dca7c8e95499181b44e
herscher <herscher@nyu.edu>
2018-05-14 21:40:23 -0400

    creating render path for filled shapes

--- Assets/scripts/Chalktalk/Curve.cs
+++ Assets/scripts/Chalktalk/Curve.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿#define DEBUG_PRINT
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Chalktalk
 {
+    public enum ChalktalkDrawType { STROKE, FILL };
+
     [RequireComponent(typeof(LineRenderer))]
     public class Curve : MonoBehaviour
     {
@@ -14,6 +18,8 @@ namespace Chalktalk
         public Color color = Color.white;
         public float width = 0f;
         public int id = 0;
+        public ChalktalkDrawType type;
+        public GameObject go;
 
         private void Awake()
         {
@@ -22,13 +28,52 @@ namespace Chalktalk
 
         public void Draw()
         {
-            line.positionCount = points.Count;
-            line.SetPositions(points.ToArray());
-            line.startColor = color;
-            line.endColor = color;
-            line.material.color = color;
-            line.startWidth = width;
-            line.endWidth = width;
+            switch (type) {
+                case ChalktalkDrawType.STROKE:
+                    line.positionCount = points.Count;
+                    line.SetPositions(points.ToArray());
+                    line.startColor = color;
+                    line.endColor = color;
+                    line.material.color = color;
+                    line.startWidth = width;
+                    line.endWidth = width;
+                    
+                    break;
+                case ChalktalkDrawType.FILL:
+                    GameObject go = this.gameObject;
+                    Mesh shape = go.GetComponent<Mesh>();
+                    MeshFilter filter = null;
+                    if (shape == null) {
+                        shape = new Mesh();
+                        go.AddComponent<MeshRenderer>();
+                        filter = go.AddComponent<MeshFilter>();
+                    } else {
+                        filter = go.GetComponent<MeshFilter>();
+                    }
+                    shape.vertices = points.ToArray();
+
+                    int countSides = points.Count;
+                    int countTris = countSides - 2;
+                    int[] indices = new int[countTris * 3];
+                    for (int i = 2; i < countSides; ++i) {
+                        indices[i - 2] = 0;
+                        indices[i - 1] = i - 1;
+                        indices[i] = i;
+                    }
+                    shape.triangles = indices;
+                    
+
+                    filter.mesh = shape;
+
+                    break;
+                default:
+                    goto case ChalktalkDrawType.STROKE;
+            }
+
+
+#if DEBUG_PRINT
+            Debug.Log(this);
+#endif
         }
 
         public override string ToString()
@@ -38,6 +83,7 @@ namespace Chalktalk
             {
                 s += " { x : " + points[i].x + " y: " + points[i].y + " z: " + points[i].z + " } ";
             }
+            s += " TYPE: " + this.type;
             return s;
         }
     }
