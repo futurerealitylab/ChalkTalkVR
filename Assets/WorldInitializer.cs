44510d308325188fad74a1e2c895fd9a360db8af
KTRosenberg <karltobyrosenberg@nyu.edu>
2018-09-13 13:16:12 -0400

    first try

---
+++ Assets/WorldInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldInitializer : MonoBehaviour {
+    public bool setAngle;
+    public float angle;
+
+    private void Awake()
+    {
+        if (setAngle) {
+            this.gameObject.transform.Rotate(new Vector3(0.0f, angle, 0.0f));
+        }
+    }
+}
