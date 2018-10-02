24332836cc8693d4a63ece9031882f6b4d71d6e7
Zhenyi He <snowymohehe@gmail.com>
2018-10-02 17:43:21 -0400

    add rift vr capture

--- Assets/scripts/VRCapture/DedicatedCmr.cs
+++ Assets/scripts/VRCapture/DedicatedCmr.cs
@@ -9,12 +9,18 @@ public class DedicatedCmr : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         vrcamera = Camera.main.transform;
-
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
+        //transform.parent.position = vrcamera.position;
+        //transform.parent.rotation = vrcamera.rotation;
+
         transform.position = vrcamera.position;
         transform.rotation = vrcamera.rotation;
+
+        //transform.localPosition = new Vector3(0, 0.6f, -0.9f);
+        //transform.localRotation = Quaternion.identity;
 	}
 }
