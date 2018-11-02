2da1968046e16e9c30ceb54b9682fa0e276cb144
KTRosenberg <ktr254@nyu.edu>
2018-11-02 13:45:05 -0400

    november 2018

---
+++ Assets/scripts/Chalktalk/CTEntityPool.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chalktalk;
+
+
+public static class CTEntityPool {
+  public static void Initialize() {
+
+  }
+
+  public static Curve getEntity(Chalktalk.ChalktalkDrawType type, Chalktalk.Renderer renderer) {
+    Curve curve = GameObject.Instantiate<Curve>(renderer.curvePrefab);
+    return curve;
+  }
+}
