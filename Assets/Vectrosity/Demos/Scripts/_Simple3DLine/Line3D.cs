82729967ab697f18c08b99d0dbfe3857af944bc7
Zhenyi He <snowymohehe@gmail.com>
2018-11-06 23:06:48 -0500

    test texts; try weld; endCap doesnot work

--- Assets/Vectrosity/Demos/Scripts/_Simple3DLine/Line3D.cs
+++ Assets/Vectrosity/Demos/Scripts/_Simple3DLine/Line3D.cs
@@ -9,42 +9,67 @@ public class Line3D : MonoBehaviour {
     public Color32[] colorOptions;
     // testing, creating multiple lines and each line has four points
 
-    List<Vector3> linePoints;
+    public List<Vector3> linePoints;
     List<Color32> lineColors;
     VectorLine line;
+    public Transform customTransform;
 
-    void CreateOneLine(int range, Color32 lineColor)
+    public Texture2D capLineTex;
+    public Texture2D capTex;
+
+    VectorLine vText;
+    public float textSize;
+    public float capLength;
+    public GameObject ori;
+    public GameObject CenterCameraEye;
+
+    void CreateOneLine(float range, Color32 lineColor)
     {
         // first point
-        Vector3 curPoint = new Vector3(Random.Range(range, range+3), Random.Range(range, range+3), Random.Range(range, range+3));
+        Vector3 curPoint = new Vector3(Random.Range(range, range + 3.0f) / 10.0f, Random.Range(0.0f, 10.0f) / 10.0f, Random.Range(range, range + 3.0f) / 10.0f);// - Camera.main.transform.forward * 10.0f;
         linePoints.Add(curPoint);
         lineColors.Add(lineColor);
         for (int i = 0; i < 3; i++)
         {
-            curPoint = new Vector3(Random.Range(range, range + 3), Random.Range(range, range + 3), Random.Range(range, range + 3));
+            curPoint = new Vector3(Random.Range(range, range + 3.0f) / 10.0f, Random.Range(0.0f, 10.0f) / 10.0f, Random.Range(range, range + 3.0f) / 10.0f);
             linePoints.Add(curPoint);
             lineColors.Add(lineColor);
             linePoints.Add(curPoint);
         }
-        curPoint = new Vector3(Random.Range(range, range + 3), Random.Range(range, range + 3), Random.Range(range, range + 3));
+        curPoint = new Vector3(Random.Range(range, range + 3.0f) / 10.0f, Random.Range(0.0f, 10.0f) / 10.0f, Random.Range(range, range + 3.0f) / 10.0f);
         linePoints.Add(curPoint);
         // continuous result with type discrete
     }
 
     // Use this for initialization
     void Start () {
+        VectorLine.SetCamera3D(CenterCameraEye);
+
         // Make a Vector2 list; in this case we just use 2 elements...
         linePoints = new List<Vector3>();
         lineColors = new List<Color32>();
         for (int j = 0; j < lineAmt; j++)
         {
             // Make a VectorLine object using the above points, with a width of 2 pixels
-            CreateOneLine(j * 3, colorOptions[j]);
+            CreateOneLine(j * 3.0f, colorOptions[j]);
         }
-        line = new VectorLine("3DLine", linePoints, 2.0f, LineType.Discrete);
+        line = new VectorLine("3DLine", linePoints, 6.0f, LineType.Discrete, Joins.Weld);
         line.SetColors(lineColors);
+        line.drawTransform = customTransform;
+        line.capLength = 10.0f;
+        VectorLine.SetEndCap("Arrow3", EndCap.Mirror, capLineTex, capTex);
+        //line.joins = Joins.Weld;
         // Draw the line
         line.Draw3D();
+        line.StopDrawing3DAuto();
+
+        
+
+        vText = new VectorLine("3DText", new List<Vector3>(), 1.0f);
+        vText.color = Color.yellow;
+        vText.drawTransform = customTransform;
+        vText.MakeText("Vectrosity!", new Vector3(0f,0.5f,0f), textSize);
+        vText.Draw3D();
     }
 	
 	// Update is called once per frame
@@ -54,5 +79,14 @@ public class Line3D : MonoBehaviour {
         //    linePoints[i] = new Vector3(Random.Range(0, 3), Random.Range(0, 3), Random.Range(0, 3));
         //}
         //line.Draw3D();
+
+        //VectorLine.SetCamera3D(ori);
+        line.capLength = capLength;
+        line.drawTransform = customTransform;
+        line.Draw3D();
+
+        vText.drawTransform = customTransform;
+        vText.MakeText("Vectrosity!", new Vector3(0.5f, 0.5f, 0.5f), textSize);
+        vText.Draw3D();
     }
 }
