8f587a7101bfca6068ad5345abc53c1cf77c60a9
Zhenyi He <snowymohehe@gmail.com>
2018-11-18 19:19:28 -0500

    disable vectrosity for video shooting, add material for gaze, add map for oculus ids

--- Assets/scripts/VRCapture/DedicatedCmr.cs
+++ Assets/scripts/VRCapture/DedicatedCmr.cs
@@ -8,6 +8,9 @@ public class DedicatedCmr : MonoBehaviour {
 
     public Vector3 offsetT;
 
+    public Vector3 staticPos;
+    public Vector3 staticRot;
+
 	// Use this for initialization
 	void Start () {
         //vrcamera = GetComponent<Camera>().transform;
@@ -19,8 +22,9 @@ public class DedicatedCmr : MonoBehaviour {
         //transform.parent.position = vrcamera.position;
         //transform.parent.rotation = vrcamera.rotation;
         Vector3 newpos = vrcamera.position + vrcamera.rotation * offsetT;
-        transform.position = Vector3.Lerp(transform.position, newpos, 0.5f);
-        //transform.position = vrcamera.position + offsetT;
+        //transform.position = Vector3.Lerp(transform.position, newpos, 0.5f);
+
+        transform.position = vrcamera.position + offsetT;
 
         Vector3 eulerAngles = vrcamera.eulerAngles;
         eulerAngles.z = 0.0f;
@@ -29,9 +33,14 @@ public class DedicatedCmr : MonoBehaviour {
         if (Vector3.Distance(transform.position, vrcamera.position) < 0.01f)
             transform.rotation = vrcamera.rotation;
         else
-          transform.LookAt(vrcamera);
+        {
+            //transform.LookAt(vrcamera);
+            transform.position = staticPos;
+            transform.rotation = Quaternion.Euler( staticRot);
+        }
+          
 
-        transform.rotation = Quaternion.Slerp(oldRotation, transform.rotation, 0.5f);
+        //transform.rotation = Quaternion.Slerp(oldRotation, transform.rotation, 0.5f);
 
         // transform.eulerAngles = Vector3.up;
         //transform.rotation =  vrcamera.rotation;
